d326a08932909e2319864e5c451cc7ee3fd00c0d
jzamora5 <jzamora_5@yahoo.com>
2019-09-18 19:03:30 +0000

    Write Function

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	char* s="write and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(2,s,65);
+	char* s="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2,s,59);
 	return (1);
 }
