2bfe43fde9fbaca12edc0a5a033902453c168af7
jzamora5 <jzamora_5@yahoo.com>
2019-09-18 17:11:51 +0000

    Printf Function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+	puts("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
