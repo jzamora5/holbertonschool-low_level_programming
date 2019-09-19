7f1b587330c09c3217eedd1c905c43588d994f45
jzamora5 <jzamora_5@yahoo.com>
2019-09-19 15:55:36 +0000

    Print Numbers 0-9 with ASCII

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n = 48;
+
+	while (n < 58)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
