9b00624e9b24b00ade00a07e213c4213a728d8b6
jzamora5 <jzamora_5@yahoo.com>
2019-09-19 15:31:15 +0000

    Print Alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n = 97;
+
+	while (n < 123)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
