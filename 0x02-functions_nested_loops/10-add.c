d1fd39584745b3975983784ea2297e8e0ff6f52b
jzamora5 <jzamora_5@yahoo.com>
2019-09-23 19:57:30 +0000

    Add two numbers Function

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * add - returns the sum of two integers
+ * @x: Integer
+ * @y: Integer
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
