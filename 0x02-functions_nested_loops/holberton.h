2b899109f2b17dc0a34f934b6a43c1757cdaf3d6
jzamora5 <jzamora_5@yahoo.com>
2019-09-24 13:35:27 +0000

    Header File

---
+++ 0x02-functions_nested_loops/holberton.h
@@ -0,0 +1,64 @@
+#ifndef _PUTCHAR_H
+#define _PUTCHAR_H
+int _putchar(char c);
+#endif
+
+#ifndef PRINT_ALPHABET_H
+#define PRINT_ALPHABET_H
+void print_alphabet(void);
+#endif
+
+#ifndef PRINT_ALPHABET_X10_H
+#define PRINT_ALPHABET_X10_H
+void print_alphabet_x10(void);
+#endif
+
+#ifndef _ISLOWER_H
+#define _ISLOWER_H
+int _islower(int c);
+#endif
+
+#ifndef _ISALPHA_H
+#define _ISALPHA_H
+int _isalpha(int c);
+#endif
+
+#ifndef PRINT_SIGN_H
+#define PRINT_SIGN_H
+int print_sign(int n);
+#endif
+
+#ifndef _ABS_H
+#define _ABS_H
+int _abs(int);
+#endif
+
+#ifndef PRINT_LAST_DIGIT_H
+#define PRINT_LAST_DIGIT_H
+int print_last_digit(int);
+#endif
+
+#ifndef JACK_BAUER
+#define JACK_BAUER
+void jack_bauer(void);
+#endif
+
+#ifndef TIMES_TABLE_H
+#define TIMES_TABLE_H
+void times_table(void);
+#endif
+
+#ifndef ADD_H
+#define ADD_H
+int add(int, int);
+#endif
+
+#ifndef PRINT_TO_98
+#define PRINT_TO_98
+void print_to_98(int n);
+#endif
+
+#ifndef PRINT_TIMES_TABLE
+#define PRINT_TIMES_TABLE
+void print_times_table(int n);
+#endif
