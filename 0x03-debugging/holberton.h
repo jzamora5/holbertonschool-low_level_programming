3aeaf65005b7453e11b440f5c83608fecd16765e
jzamora5 <jzamora_5@yahoo.com>
2019-09-24 16:28:41 +0000

    Header

---
+++ 0x03-debugging/holberton.h
@@ -0,0 +1,20 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+#include <stdio.h>
+void positive_or_negative(int i);
+#endif /* HOLBERTON_H */
+
+#ifndef LARGEST_NUMBER_H
+#define LARGEST_NUMBER_H
+int largest_number(int a, int b, int c);
+#endif
+
+#ifndef CONVERT_DAY_H
+#define CONVERT_DAY_H
+int convert_day(int month, int day);
+#endif
+
+#ifndef PRINT_REMAINING_DAYS_H
+#define PRINT_REMAINING_DAYS_H
+void print_remaining_days(int month, int day, int year);
+#endif
