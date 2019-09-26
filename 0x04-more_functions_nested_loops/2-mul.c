9f0f316e0c0a75f1d920426a8a9549666ac9233a
jzamora5 <jzamora_5@yahoo.com>
2019-09-26 14:39:23 +0000

    Multiplication Function

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: Input parameter Integer
+ * @b: Input parameter Integer
+int mul(int a, int b) * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
