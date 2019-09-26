95abac57526fa0d89846d85b1444bce825e20ab9
jzamora5 <jzamora_5@yahoo.com>
2019-09-26 17:05:53 +0000

    Print Number Putchar

---
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -0,0 +1,59 @@
+#ifndef _PUTCHAR_H
+#define _PUTCHAR_H
+int _putchar(char c);
+#endif
+
+#ifndef _ISUPPER_H
+#define _ISUPPER_H
+int _isupper(int c);
+#endif
+
+#ifndef _ISDIGIT_H
+#define _ISDIGIT_H
+int _isdigit(int c);
+#endif
+
+#ifndef MUL_H
+#define MUL_H
+int mul(int a, int b);
+#endif
+
+#ifndef PRINT_NUMBERS_H
+#define PRINT_NUMBERS_H
+void print_numbers(void);
+#endif
+
+#ifndef PRINT_MOST_NUMBERS_H
+#define PRINT_MOST_NUMBERS_H
+void print_most_numbers(void);
+#endif
+
+#ifndef MORE_NUMBERS_H
+#define MORE_NUMBERS_H
+void more_numbers(void);
+#endif
+
+#ifndef PRINT_LINE_H
+#define PRINT_LINE_H
+void print_line(int n);
+#endif
+
+#ifndef PRINT_DIAGONAL_H
+#define PRINT_DIAGONAL_H
+void print_diagonal(int n);
+#endif
+
+#ifndef PRINT_SQUARE_H
+#define PRINT_SQUARE_H
+void print_square(int size);
+#endif
+
+#ifndef PRINT_TRIANGLE_H
+#define PRINT_TRIANGLE_H
+void print_triangle(int size);
+#endif
+
+#ifndef PRINT_NUMBER_H
+#define PRINT_NUMBER_H
+void print_number(int n);
+#endif
