9512c392f3c9cc152d22515c6ea4b96dc20d8cdb
jzamora5 <jzamora_5@yahoo.com>
2019-09-30 16:59:33 +0000

    Holberton Header

---
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -0,0 +1,14 @@
+#ifndef RESET_TO_98_H
+#define RESET_TO_98_H
+void reset_to_98(int *n);
+#endif
+
+#ifndef SWAP_INT_H
+#define SWAP_INT_H
+void swap_int(int *a, int *b);
+#endif
+
+#ifndef _STRLEN_H
+#define _STRLEN_H
+int _strlen(char *s);
+#endif
