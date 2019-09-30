f4a56129a98ef4e83cfdce4abf792f65cb84e0b1
jzamora5 <jzamora_5@yahoo.com>
2019-09-30 23:44:16 +0000

    Header File

--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -1,3 +1,8 @@
+#ifndef _PUTCHAR_H
+#define _PUTCHAR_H
+int _putchar(char c);
+#endif
+
 #ifndef RESET_TO_98_H
 #define RESET_TO_98_H
 void reset_to_98(int *n);
@@ -11,4 +16,44 @@ void swap_int(int *a, int *b);
 #ifndef _STRLEN_H
 #define _STRLEN_H
 int _strlen(char *s);
+#endif
+
+#ifndef _PUTS_H
+#define _PUTS_H
+void _puts(char *str);
+#endif
+
+#ifndef _PRINT_REV_H
+#define _PRINT_REV_H
+void print_rev(char *s);
+#endif
+
+#ifndef _REV_STRING_H
+#define _REV_STRING_H
+void rev_string(char *s);
+#endif
+
+#ifndef PUTS2_H
+#define PUTS2_H
+void puts2(char *str);
+#endif
+
+#ifndef PUTS_HALF
+#define PUTS_HALF
+void puts_half(char *str);
+#endif
+
+#ifndef PRINT_ARRAY_H
+#define PRINT_ARRAY_H
+void print_array(int *a, int n);
+#endif
+
+#ifndef _STRCPY
+#define _STRCPY
+char *_strcpy(char *dest, char *src);
+#endif
+
+#ifndef _ATOI_H
+#define _ATOI_H
+int _atoi(char *s);
 #endif
