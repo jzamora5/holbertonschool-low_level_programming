8e92b9e8a31d698c50e02ab79eef3284cb062de9
jzamora5 <jzamora_5@yahoo.com>
2019-10-01 13:33:43 +0000

    Print String in Reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,23 +2,23 @@
 
 /**
  * print_rev - prints a string in reverse
- * @str: string
+ * @s: string
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-void print_rev(char *str)
+void print_rev(char *s)
 {
 	int i;
 
 	i = 0;
-	while (str[i] != '\0')
+	while (s[i] != '\0')
 	{
 		i++;
 	}
 	while (i >= 0)
 	{
-		_putchar(str[i]);
+		_putchar(s[i]);
 		i--;
 	}
 	_putchar('\n');
