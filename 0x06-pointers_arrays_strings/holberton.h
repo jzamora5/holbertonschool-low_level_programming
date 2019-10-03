6d250f758bb6faad57e088a4bd077e9a810fc796
jzamora5 <jzamora_5@yahoo.com>
2019-10-03 17:02:06 +0000

    Header File

---
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -0,0 +1,54 @@
+#ifndef _PUTCHAR_H
+#define _PUTCHAR_H
+int _putchar(char c);
+#endif
+
+#ifndef _STRCAT_H
+#define _STRCAT_H
+char *_strcat(char *dest, char *src);
+#endif
+
+#ifndef _STRNCAT_H
+#define _STRNCAT_H
+char *_strncat(char *dest, char *src, int n);
+#endif
+
+#ifndef _STRNCPY_H
+#define _STRNCPY_H
+char *_strncpy(char *dest, char *src, int n);
+#endif
+
+#ifndef _STRNCPY_H
+#define _STRNCPY_H
+char *_strncpy(char *dest, char *src, int n);
+#endif
+
+#ifndef _STRCMP_H
+#define _STRCMP_H
+int _strcmp(char *s1, char *s2);
+#endif
+
+#ifndef _STRCMP_H
+#define _STRCMP_H
+int _strcmp(char *s1, char *s2);
+#endif
+
+#ifndef _REVERSE_ARRAY_H
+#define _REVERSE_ARRAY_H
+void reverse_array(int *a, int n);
+#endif
+
+#ifndef _STRING_TOUPPER_H
+#define _STRING_TOUPPER_H
+char *string_toupper(char *);
+#endif
+
+#ifndef _CAP_STRING_H
+#define _CAP_STRING_H
+char *cap_string(char *);
+#endif
+
+#ifndef LEET_H
+#define LEET_H
+char *leet(char *);
+#endif
