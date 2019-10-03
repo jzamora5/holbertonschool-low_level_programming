07255f34caadcfbba87be0ff6495bf4bf600e77d
jzamora5 <jzamora_5@yahoo.com>
2019-10-03 18:01:40 +0000

    1337 Encode

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * leet - encodes string to 1337
+ * @s: string to encode
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+char *leet(char *s)
+{
+	int i, j;
+
+	char leet[]="aeotl";
+	char leet2[]="AEOTL";
+	char leet3[]="43071";
+	j = 0;
+	while(*s != 0)
+	{
+		for (i = 0; i <= 4; i++)
+		{
+			if (s[i] == leet[i] || s[i] == leet2[i])
+				s[i] = leet3[i];
+		}
+		s++;
+		j++;
+	}
+	s = s - j;
+	return (s);
+}
