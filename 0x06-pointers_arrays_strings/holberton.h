40a6c62f25dfec248dfe011b2c6968248680f73d
jzamora5 <jzamora_5@yahoo.com>
2019-10-03 20:41:30 +0000

    Header File

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -57,3 +57,13 @@ char *leet(char *);
 #define ROT13_H
 char *rot13(char *);
 #endif
+
+#ifndef INFINITE_ADD_H
+#define INFINITE_ADD_H
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+#endif
+
+#ifndef PRINT_NUMBER_H
+#define PRINT_NUMBER_H
+void print_number(int n);
+#endif
