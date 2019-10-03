069414de6cec7a440374746361079be32457d753
jzamora5 <jzamora_5@yahoo.com>
2019-10-03 17:24:59 +0000

    Compare Strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -14,15 +14,11 @@ int _strcmp(char *s1, char *s2)
 	while (*s1 != '\0')
 	{
 		if (*s2 == '\0')
-			return (1);
-		if (*s2 > *s1)
-			return (-1);
-		if (*s1 > *s2)
-			return (1);
+			return (15);
 		s1++;
 		s2++;
 	}
 	if (*s2 != '\0')
-		return (-1);
+		return (-15);
 	return (0);
 }
