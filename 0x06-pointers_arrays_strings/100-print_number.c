f2c51eba2b8e44811e28be5649e6a753b8514d12
jzamora5 <jzamora_5@yahoo.com>
2019-10-03 20:40:57 +0000

    Print Number

---
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+
+/**
+ * print_number - prints a number with _putchar
+ * @n: Number to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void print_number(int n)
+{
+	int ten, i, neg;
+	unsigned int digit, digit2;
+
+	ten = 1;
+	if (n < 0)
+	{
+		n = n * -1;
+		neg = 1;
+	}
+	digit = n;
+	digit2 = n;
+	while (digit >= 10)
+	{
+		digit = digit / 10;
+		ten = ten * 10;
+	}
+	if (neg == 1)
+	{
+		_putchar('-');
+	}
+	_putchar('0' + (digit2 / ten));
+	i = ten / 10;
+	while (i >= 1)
+	{
+		_putchar('0' + (digit2 / i) % 10);
+		i = i / 10;
+	}
+}
