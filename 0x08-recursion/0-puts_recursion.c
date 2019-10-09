b9155a51a7131dce61b35ae309cd083f776f49de
jzamora5 <jzamora_5@yahoo.com>
2019-10-09 13:54:36 +0000

    Print String

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _puts_recursion - Prints a string followed by a new line
+ * @s: memory area
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s+1);
+}
