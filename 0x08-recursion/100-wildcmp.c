04bd4cc49e8bf2a206b29c539ee0fd2f38c8d695
jzamora5 <jzamora_5@yahoo.com>
2019-10-09 20:35:26 +0000

    Wildcard

---
+++ 0x08-recursion/100-wildcmp.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * wildcmp - compares two strings and returns 1 if the strings
+ * can be considered identical, otherwise return 0.
+ * @s1: string to compare to
+ * @s2: string with wild character
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+
+	if (*s2 == '*')
+	{
+		if (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2))
+			return (1);
+	}
+
+	return (0);
+}
