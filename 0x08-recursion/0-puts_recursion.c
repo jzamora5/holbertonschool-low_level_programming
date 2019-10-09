589328af04808863fa27c2bfde84fec66e06df9e
jzamora5 <jzamora_5@yahoo.com>
2019-10-09 13:57:46 +0000

    Print String

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,7 +2,7 @@
 
 /**
  * _puts_recursion - Prints a string followed by a new line
- * @s: memory area
+ * @s: string
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
