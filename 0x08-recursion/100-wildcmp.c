7c51a008e247851c7b06f69043025610d9470b3e
jzamora5 <jzamora_5@yahoo.com>
2019-10-09 21:36:02 +0000

    Wildcard

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -21,6 +21,8 @@ int wildcmp(char *s1, char *s2)
 	{
 		if (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2))
 			return (1);
+		if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
+			return (0);
 	}
 
 	return (0);
