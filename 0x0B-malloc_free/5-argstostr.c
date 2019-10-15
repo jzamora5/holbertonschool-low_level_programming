09f2708a7a3a487599b5d18ad2d2dacb679fff8f
jzamora5 <jzamora_5@yahoo.com>
2019-10-15 16:48:50 +0000

    Concatenates Main Arguments

---
+++ 0x0B-malloc_free/5-argstostr.c
@@ -0,0 +1,47 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * argstostr - concatenates all the arguments of your program
+ * @ac: argument count in main
+ * @av: arguments passed to main
+ *
+ * Return: Pointer
+ */
+char *argstostr(int ac, char **av)
+{
+	if (ac == 0 || av == NULL)
+		return (0);
+	char *s;
+	int l, lt, i, j, k;
+
+	l = 0, k = 0;
+	for (i = 0; i < ac; i++)
+	{
+		lt = 0;
+		while (av[i][lt])
+			lt++;
+		l += lt + 1;
+	}
+	s = malloc((l + 1) * sizeof(char));
+
+	if (s == 0)
+		return (0);
+
+	for (j = 0; j < ac; j++)
+	{
+		lt = 0;
+		while (av[j][lt])
+		{
+			*(s + k) = av[j][lt];
+			k++;
+			lt++;
+		}
+		*(s + k) = '\n';
+		k++;
+	}
+	*(s + k) = '\0';
+
+	return (s);
+}
