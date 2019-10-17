f56e9fa380bbded62ef2d214bb133a8aedfff807
jzamora5 <jzamora_5@yahoo.com>
2019-10-17 18:10:01 +0000

    Custom Realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -15,7 +15,7 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
-	for (i = 0; i < n; i++, dest++, src++)
+	for (i = 0; i < n; i++)
 		dest[i] = src[i];
 	return (dest);
 }
