2abfff1a62dc475fa7bc72b28d386986f6c2a267
jzamora5 <jzamora_5@yahoo.com>
2019-10-21 19:31:50 +0000

    [FIX] Create Struct from Function

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,11 +11,24 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	int ln = 0, lo = 0, i, j;
+	char *namec, *ownerc;
 	dog_t *new_dog;
 
 	new_dog = malloc(sizeof(struct dog));
-	if (new_dog == 0 || name == 0 || owner == 0)
+
+	while (name[ln] != 0)
+		ln++;
+	while (owner[lo] != 0)
+		lo++;
+	namec = malloc(ln + 1);
+	ownerc = malloc(lo + 1);
+	if (new_dog == 0 || namec == 0 || ownerc == 0)
 		return (0);
+	for (i = 0; i <= ln; i++)
+		namec[i] = name[i];
+	for (j = 0; j <= lo; j++)
+		ownerc[j] = owner[j];
 	new_dog->name = name;
 	new_dog->age = age;
 	new_dog->owner = owner;
