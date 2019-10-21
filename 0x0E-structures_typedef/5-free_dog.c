7ea686401d1212ac852f7bf0fbafc7951cb76216
jzamora5 <jzamora_5@yahoo.com>
2019-10-21 20:22:58 +0000

    [EDIT] Header

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -9,7 +9,10 @@
  */
 void free_dog(dog_t *d)
 {
-	free(d->name);
-	free(d->owner);
-	free(d);
+	if (d->name != 0)
+		free(d->name);
+	if (d->owner != 0)
+		free(d->owner);
+	if (d != 0)
+		free(d);
 }
