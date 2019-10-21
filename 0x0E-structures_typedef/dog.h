603ad05ca0683aaade197f0856c58b606d92ca17
jzamora5 <jzamora_5@yahoo.com>
2019-10-21 18:51:11 +0000

    [EDIT] Header

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -20,4 +20,5 @@ typedef struct dog
 int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
 #endif
