8187c27d68748b7f822e6343a61a612021b65539
jzamora5 <jzamora_5@yahoo.com>
2019-10-21 18:20:15 +0000

    [FIX]

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,5 @@
 #include "dog.h"
-
+#include <stdlib.h>
 /**
  * init_dog - writes the character c to stdout
  * @d: Dog
@@ -12,7 +12,10 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	if (d != 0)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
