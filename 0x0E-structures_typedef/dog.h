726af9303bcac00bebe4d6101a84cb1a7e145edd
jzamora5 <jzamora_5@yahoo.com>
2019-10-21 18:03:39 +0000

    Struct in Header

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef STRUCTS
+#define STRUCTS
+/**
+ * struct dog - description for a pet
+ * @name: pet name
+ * @age: pet age
+ * @owner: owner
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
+#ifndef _FUNCTIONS_H
+#define _FUNCTIONS_H
+int _putchar(char c);
+#endif
