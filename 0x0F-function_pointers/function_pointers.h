4a24aa5d66df972a719efa00350dc91a88382f56
jzamora5 <jzamora_5@yahoo.com>
2019-10-23 17:09:41 +0000

    [FEATURE] Header File for Functions File: function_pointers.h

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,7 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+typedef __SIZE_TYPE__ size_t;
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+#endif
