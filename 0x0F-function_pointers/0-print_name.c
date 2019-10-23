43fdbdff3cf659978a170bb60cb0eae4c4090f7d
jzamora5 <jzamora_5@yahoo.com>
2019-10-23 14:15:42 +0000

    [Feature] function that prints a name File: 0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - writes the character c to stdout
+ * @name: String
+ * @f: Pointer to function with char parameter and no return
+ *
+ * Return: Nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
