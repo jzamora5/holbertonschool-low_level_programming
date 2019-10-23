ef154375e9719c4cebf6b411262bc9f8a9ef2c22
jzamora5 <jzamora_5@yahoo.com>
2019-10-23 14:52:29 +0000

    [FEATURE] function that searches for an integer File: 2-int_index.c

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for an integer
+ * @array: array of type int
+ * @size: size of array
+ * @cmp: function with argument int and no return
+ *
+ * Return: No element matches -1, if size <=0 -1
+ * else index of first element cmp func does not rtrn 0
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i, f = 0;
+
+	if (size <= 0)
+		return (-1);
+	for (i = 0; i < size && f == 0; i++)
+		f = cmp(array[i]);
+
+	if (i == size)
+		i = 0;
+
+	return (i - 1);
+}
