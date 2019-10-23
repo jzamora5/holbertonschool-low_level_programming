4772eb2526051c2dbc735c792712e670e0b48b36
jzamora5 <jzamora_5@yahoo.com>
2019-10-23 19:52:23 +0000

    [FIX] Cast Type of Main Adress

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -12,7 +12,7 @@
 int main(int argc, char **argv)
 {
 	int i, bytes;
-	int *mainaddr;
+	char *mainaddr;
 
 	if (argc != 2)
 		printf("Error\n"), exit(1);
@@ -22,7 +22,7 @@ int main(int argc, char **argv)
 	if (bytes < 0)
 		printf("Error\n"), exit(2);
 
-	mainaddr =  (int *)main;
+	mainaddr = (char *)main;
 
 	for (i = 0; i < bytes - 1; i++)
 		printf("%02hhx ", mainaddr[i]);
