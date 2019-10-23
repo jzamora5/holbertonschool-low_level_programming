36719e47d5a6b216dd8fc3389b87fc5acaf02ded
jzamora5 <jzamora_5@yahoo.com>
2019-10-23 17:27:20 +0000

    [FIX] Null

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,12 +13,10 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (cmp || size <= 0 || array)
-		return (-1);
-
-	for (i = 0; i < size; i++)
-		if(cmp(array[i]))
-			return (i);
+	if (array && cmp && size > 0)
+		for (i = 0; i < size; i++)
+			if (cmp(array[i]))
+				return (i);
 
 	return (-1);
 }
