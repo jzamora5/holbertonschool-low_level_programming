5d8d217719e136bfecf02d8d02770367c1e54cf2
jzamora5 <jzamora_5@yahoo.com>
2019-10-25 18:36:47 +0000

    [Feature] 64-bit program in assembly that prints Hello, Holberton, followed by a new line File: 100-hello_holberton.asm

---
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -0,0 +1,15 @@
+	SECTION .data
+msg:	 db "Hello, Holberton", 10 ;message, use 10 for new line
+
+	SECTION .text
+	global main 		;declaration of main function for gcc
+main:
+	mov rdx, 17 		;third argument: length of string
+	mov rcx, msg		;second argument: pointer to message to write
+	mov rbx, 1		;first argument: file handle (stdout)
+	mov rax, 4		;system call numer (sys_write)
+	int 0x80		;call kernel (interrupt)
+
+	mov rbx, 0		;move 0 to rbx (exit code)
+	mov rax, 1		;system call for exit
+	int 0x80		;call kernel (interrupt)
