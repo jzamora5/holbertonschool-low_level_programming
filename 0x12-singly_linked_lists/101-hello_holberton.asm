467d1afc59c8bfbd1e12cf3129a5b1950c395174
jzamora5 <jzamora_5@yahoo.com>
2019-10-31 18:52:09 +0000

    [Printf] Added %s method

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,16 +1,17 @@
 section .rodata
-    msg:    db 'Hello, Holberton', 10
-    msglen: equ $-msg
+    format: db 'Hello %s', 10, 0
+    name:   db 'Holberton', 0
 
 section .text
         global main
+        extern printf
     main:
-        ; write(1, msg, msglen)
-        mov rdi, 1
-        mov rsi, msg
-        mov rdx, msglen
-        mov rax, 1
-        syscall
+        ; printf(format, name)
+        mov rdi, format
+        mov rsi, name
+        ; no XMM registers
+        mov rax, 0
+        call printf
         ; return 0
         mov rax, 0
         ret
