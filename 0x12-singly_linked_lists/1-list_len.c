baaee9471188477a7f19d6a62df702817d7b1631
jzamora5 <jzamora_5@yahoo.com>
2019-10-31 15:10:56 +0000

    [Update] Added Library Definition

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,5 +1,5 @@
+#include <stdio.h>
 #include "lists.h"
-#include <stddef.h>
 
 /**
  * list_len - returns the number of elements in a linked list_t list
@@ -12,7 +12,7 @@ size_t list_len(const list_t *h)
 {
 	unsigned int size = 0;
 
-	while (h != NULL)
+	while (h != 0)
 		h = h->next, size++;
 
 	return (size);
