aed549e73445b96c834308112544c88f9f0782d6
jzamora5 <jzamora_5@yahoo.com>
2019-10-31 18:11:55 +0000

    [Fix] Betty

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -9,5 +9,6 @@ void msg(void)__attribute__((constructor));
  */
 void msg(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,");
+	printf("\nI bore my house upon my back!\n");
 }
