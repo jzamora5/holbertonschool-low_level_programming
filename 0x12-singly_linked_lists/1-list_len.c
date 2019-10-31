e18dbb037ba54a568fe1b17cb551ddaf82ff66b7
jzamora5 <jzamora_5@yahoo.com>
2019-10-31 14:10:17 +0000

    [Add] Function to get length of Singly Linked List

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "lists.h"
+#include <stddef.h>
+
+/**
+ * list_len - returns the number of elements in a linked list_t list
+ * @h: pointer to head of singly linked list
+ *
+ * Return: Number of elements in list
+ */
+
+size_t list_len(const list_t *h)
+{
+	unsigned int size = 0;
+
+	while (h != NULL)
+		h = h->next, size++;
+
+	return (size);
+}
