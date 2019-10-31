a37b406908ceeedc475f51d4f1b2446b682ee25a
jzamora5 <jzamora_5@yahoo.com>
2019-10-31 18:10:36 +0000

    [Add] Constructor

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void msg(void)__attribute__((constructor));
+
+/**
+ * msg - prints a specific message
+ *
+ * Return: No return
+ */
+void msg(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
