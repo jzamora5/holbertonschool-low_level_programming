a6276d4e0989cb0e3bbac7f3f2c4350758441fcb
jzamora5 <jzamora_5@yahoo.com>
2019-11-05 14:20:19 +0000

    [Fix] Declaration before check

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -10,12 +10,13 @@
 int pop_listint(listint_t **head)
 {
 	listint_t *tmp = *head;
-	listint_t *tmp2 = tmp->next;
+	listint_t *tmp2;
 	int n;
 
 	if (head == 0 || tmp == 0)
 		return (0);
 
+	tmp2 = tmp->next;
 	n = tmp->n;
 	free(tmp);
 	*head = tmp2;
