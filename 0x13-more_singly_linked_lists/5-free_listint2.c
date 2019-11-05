33e07f8e0794597dc6b4f0292e241de049807050
jzamora5 <jzamora_5@yahoo.com>
2019-11-05 14:01:30 +0000

    [Fix] Head Pointer Check

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -11,12 +11,14 @@ void free_listint2(listint_t **head)
 {
 	listint_t *tmp;
 
-
-	while (*head != 0)
+	if (head)
 	{
-		tmp = *head;
-		*head = tmp->next;
-		free(tmp);
+		while (*head != 0)
+		{
+			tmp = *head;
+			*head = tmp->next;
+			free(tmp);
+		}
 	}
 
 }
