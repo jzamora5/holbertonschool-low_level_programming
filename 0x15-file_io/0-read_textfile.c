a8a161302d47dc3c2430a4f8a4e350b0e1a868eb
jzamora5 <jzamora_5@yahoo.com>
2019-11-12 15:03:02 +0000

    [ADD] function that reads a text file and prints it to the POSIX standard output

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,51 @@
+#include "holberton.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+/**
+ * read_textfile -  reads a text file and prints it
+ * to the POSIX standard output
+ * @filename: name of the file
+ * @letters: number of letters it should read and print
+ *
+ * Return: returns the actual number of letters it could read and print
+ * 0 if file cannot be opened or read
+ * 0 if filename is NULL
+ * 0 if write fails or does not write expected amount of bytes
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, checkr, checkw;
+	char *c;
+
+	if (filename == 0)
+		return (0);
+
+	c = malloc(letters + 1);
+
+	if (c == 0)
+		return (0);
+
+	fd  = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (free(c), 0);
+
+	checkr = read(fd, c, letters);
+
+	if (checkr == -1)
+		return (free(c), 0);
+
+	c[letters] = '\0';
+
+	checkw = write(STDIN_FILENO, c, checkr);
+	if (checkw == -1)
+		return (free(c), 0);
+
+	free(c);
+	close(fd);
+	return (checkw);
+}
