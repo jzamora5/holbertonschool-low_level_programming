946687ce4be87aab2915232c23f4980ac8c398ca
jzamora5 <jzamora_5@yahoo.com>
2019-11-12 17:59:58 +0000

    Test

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,19 +22,18 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd, checkw, l = 0;
 
-	while (text_content[l] != 0)
-		l++;
-
 	if (filename == 0)
 		return (-1);
 
-	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 
 	if (fd == -1)
 		return (-1);
 
 	if (text_content)
 	{
+		while (text_content[l] != 0)
+			l++;
 		checkw = write(fd, text_content, l);
 
 		if (checkw == -1 || checkw != l)
