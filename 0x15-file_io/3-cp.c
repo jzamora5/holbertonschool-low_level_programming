2e028cd47a533b444e6a1fc8ca86f9eb2c2f843e
jzamora5 <jzamora_5@yahoo.com>
2019-11-12 18:42:02 +0000

    Test

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -34,12 +34,13 @@ int main(int argc, char **argv)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 
 	fdfrom = open(argv[1], O_RDONLY);
-
 	if (fdfrom == -1)
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]), exit(98);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
 
 	fdto = open(argv[2], O_RDWR | O_CREAT | O_TRUNC, 0664);
-
 	if (fdto == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 
