f3985ba6c69b4b339871570999fcf86fd38fe8cc
jzamora5 <jzamora_5@yahoo.com>
2019-11-12 15:44:54 +0000

    function that creates a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,46 @@
+#include "holberton.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+/**
+ * create_file - creates a file
+ *
+ * @filename: name of the file
+ * @text_content: NULL terminated string to write to the file
+ *
+ * Return: Returns: 1 on success, -1 on failure
+ * file can not be created, file can not be written, write “fails”, etc…
+ * The created file must have those permissions: rw-------.
+ * If the file already exists, do not change the permissions.
+ * if filename is NULL return -1
+ * if text_content is NULL create an empty file
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, checkw, l = 0;
+
+	while (text_content[l] != 0)
+		l++;
+
+	if (filename == 0)
+		return (-1);
+
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+
+	if (fd == -1)
+		return (-1);
+
+	if (text_content)
+	{
+		checkw = write(fd, text_content, l);
+
+		if (checkw == -1)
+			return (-1);
+	}
+
+	close(fd);
+	return (1);
+}
