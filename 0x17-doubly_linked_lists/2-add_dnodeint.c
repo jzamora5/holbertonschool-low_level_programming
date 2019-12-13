8838b9a75d27da7adae9fceb8e90fac442762cd2
jzamora5 <jzamora_5@yahoo.com>
2019-12-13 15:12:43 +0000

    Fix Missing Node->prev assigment

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -24,6 +24,8 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 	new_node->prev = NULL;
 	new_node->next = *head;
+	if (*head)
+		(*head)->prev = new_node;
 	*head = new_node;
 
 	return (new_node);
