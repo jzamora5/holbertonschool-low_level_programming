e4ec46f3612144652b3a4b393bed009f0d06c689
jzamora5 <jzamora_5@yahoo.com>
2019-12-16 20:04:26 +0000

    Raw Link

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://github.com/jzamora5/holbertonschool-low_level_programming/blob/master/0x18-dynamic_libraries/jackpot.so
+wget -qO /tmp/jackpot.so https://github.com/jzamora5/holbertonschool-low_level_programming/raw/master/0x18-dynamic_libraries/jackpot.so
 export LD_PRELOAD=/tmp/jackpot.so
