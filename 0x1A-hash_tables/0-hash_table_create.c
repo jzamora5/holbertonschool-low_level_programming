75eda8e77022bf4d4ac5b574760ccc4140667fa4
jzamora5 <jzamora_5@yahoo.com>
2020-01-18 16:16:32 +0000

    FIX Calloc Size

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -16,7 +16,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 
 	hash_table->size = size;
-	hash_table->array = calloc(size, sizeof(hash_node_t));
+	hash_table->array = calloc(size, sizeof(hash_node_t *));
 	if (!(hash_table->array))
 	{
 		free(hash_table);
