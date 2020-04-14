cd82bf6aa8bd40974b6e8708d42ccf66b86b2724
jzamora5 <jzamora_5@yahoo.com>
2020-04-14 18:51:28 +0000

    Binary Search

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,52 @@
+#include "search_algos.h"
+
+/**
+ * print_array - Prints an array between two boundaries
+ * @array: pointer to the first element of the array to print
+ * @min: left boundary
+ * @max: right boundary
+ *
+ * Return: No Return
+ */
+void print_array(int *array, int min, int max)
+{
+	int i;
+
+	for (i = min; i < max; i++)
+		printf("%d, ", array[i]);
+
+	printf("%d\n", array[i]);
+}
+
+/**
+ * binary_search - searches for a value in a sorted array of integers using the
+ * Binary search algorithm
+ * @array: pointer to the first element of the array to search in
+ * @size:  number of elements in array
+ * @value: value to search for
+ *
+ * Return: Index where value is located or -1
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	size_t min, max, mid;
+
+	if (!array)
+		return (-1);
+
+	min = 0, max = size - 1;
+	while (min <= max)
+	{
+		printf("Searching in array: ");
+		print_array(array, min, max);
+		mid = (min + max) / 2;
+		if (array[mid] < value)
+			min = mid + 1;
+		else if (array[mid] > value)
+			max = mid - 1;
+		else
+			return (mid);
+	}
+
+	return (-1);
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -6,5 +6,6 @@
 
 /** Prototypes **/
 int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
 
 #endif /* _SEARCH_ALGOS_H_ */
