981c7c96c08c10db2aadd95b2a8dc18ebc17d35a
jzamora5 <jzamora_5@yahoo.com>
2020-04-14 22:04:48 +0000

    Update Header

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -7,5 +7,8 @@
 /** Prototypes **/
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
 
 #endif /* _SEARCH_ALGOS_H_ */
