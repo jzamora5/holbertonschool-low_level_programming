da8eaa1161803ac7fd43972288e080e0aa1284e2
Zakaria Cheikh <133505628+ezacaryy@users.noreply.github.com>
2024-01-29 18:08:52 +0100

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,19 +1,19 @@
-#include "holberton.h"
-
+#include "main.h"
 /**
- * _puts_recursion - Prints a string followed by a new line
- * @s: string
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * _puts_recursion - function like puts();
+ * @s: input
+ * Return: Always 0 (Success)
  */
+
 void _puts_recursion(char *s)
 {
-	if (*s == 0)
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
 	{
 		_putchar('\n');
-		return;
 	}
-	_putchar(*s);
-	_puts_recursion(s + 1);
 }
